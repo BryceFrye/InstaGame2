6b39eba35df6be1b457ed7ba384217ea4e7f629d
Bryce <bcfrye19@gmail.com>
2012-04-16 16:54:45 -0700

    test

--- app.js
+++ app.js
@@ -72,7 +72,6 @@ app.get('/', function(req, res){
         var parsedJSON = eval("(function(){return " + chunk + ";})()");
         var token = parsedJSON.access_token;
         console.log("TOKEN: "+ token);
-        render(res, token);
       });
     });
     post_req.write(post_data);
