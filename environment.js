221aba1e93072aa116beaba34a1606d29426f2ef
Bryce <bcfrye19@gmail.com>
2012-12-06 22:48:52 -0800

    prepared for deploy

---
+++ environment.js
@@ -0,0 +1,19 @@
+module.exports = function(){
+	
+	//~~~ PRODUCTION ~~~//
+	MY_CLIENT_ID = "7ef880e896434566ba789a50d73ae204";
+	MY_CLIENT_SECRET = "f82712c0f4e848ae935b103947351321";
+	MY_REDIRECT_URI = "http://instagame.herokuapp.com/";
+	MY_HOST = "http://instagame.herokuapp.com/";
+	
+	
+	//~~~ TEST ~~~//
+	// MY_CLIENT_ID = "3a87507d4e284471be0b3fe831c80a18";
+	// MY_CLIENT_SECRET = "0e99cc12871146a49b7f161730fd3a0b";
+	// MY_REDIRECT_URI = "http://www.localhost.com:3030";
+	// MY_HOST = "localhost";
+	
+	
+	//~~~ BOTH ~~~//
+	MY_LOGIN_REDIRECT = 'https://api.instagram.com/oauth/authorize/?client_id='+MY_CLIENT_ID+'&redirect_uri='+MY_REDIRECT_URI+'&scope=relationships&response_type=code';
+}
